7047778ec46d32f804837b4dfb73ddc906e015b7
Abaquita <abaquitakyo49@gmail.com>
2025-05-21 07:45:35 +0800

    cors allignment

--- Sprint-1/backend/config/cors.php
+++ Sprint-1/backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','https://latest-sprint.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000','https://pmsd.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
